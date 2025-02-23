1675909c69698afc648c3703a6914cb1c0a629a2
Ernest Przybył <ernestprzybyl@gmail.com>
2025-02-23 13:10:37 +0100

    fix db performance issues

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,15 +1,9 @@
-import * as migration_20250125_110705 from './20250125_110705';
-import * as migration_20250222_174721_about_me_first_setup from './20250222_174721_about_me_first_setup';
+import * as migration_20250223_120726 from './20250223_120726';
 
 export const migrations = [
   {
-    up: migration_20250125_110705.up,
-    down: migration_20250125_110705.down,
-    name: '20250125_110705',
-  },
-  {
-    up: migration_20250222_174721_about_me_first_setup.up,
-    down: migration_20250222_174721_about_me_first_setup.down,
-    name: '20250222_174721_about_me_first_setup'
+    up: migration_20250223_120726.up,
+    down: migration_20250223_120726.down,
+    name: '20250223_120726'
   },
 ];
