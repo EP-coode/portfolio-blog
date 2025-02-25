e086d1ec33aab18304dc36bc8a5783387ebd240e
Ernest Przybył <ernestprzybyl@gmail.com>
2025-02-25 21:37:48 +0100

    add dates localization

--- src/blocks/Timeline/TimeineBlock.tsx
+++ src/blocks/Timeline/TimeineBlock.tsx
@@ -2,16 +2,24 @@
 
 import React from 'react'
 
-import { TimeLine as TimelineBlockProps } from '@/payload-types'
-
-import { RichText as RichTextWithoutBlocks } from '@payloadcms/richtext-lexical/react'
-
-import styles from './TimelineBlock.module.css'
-import { useIntersectionObserver } from '@/modules/common/hooks/useIntersectionObserver'
-import clsx from 'clsx'
 import Link from 'next/link'
 
+import { TimeLine as TimelineBlockProps } from '@/payload-types'
+import { RichText as RichTextWithoutBlocks } from '@payloadcms/richtext-lexical/react'
+import clsx from 'clsx'
 import { format } from 'date-fns'
+import { pl, enUS } from 'date-fns/locale'
+import { useLocale, useTranslations } from 'next-intl'
+import { Locale } from '@/i18n/routing'
+
+import { useIntersectionObserver } from '@/modules/common/hooks/useIntersectionObserver'
+
+import styles from './TimelineBlock.module.css'
+
+const localeMap = {
+  pl: pl,
+  en: enUS,
+}
 
 type TimeLineItemProps = NonNullable<TimelineBlockProps['timeLineItems']>[number]
 const DATE_FORMAT = 'MMM yyyy'
@@ -40,13 +48,19 @@ const TimelineBlockItem = ({
   companyName,
   companyLink,
 }: TimeLineItemProps) => {
+  const locale = useLocale() as Locale
+  const t = useTranslations('timeline')
   const { isIntersecting, ref } = useIntersectionObserver({
     threshold: 0.3,
     rootMargin: '-10%',
   })
 
-  const startDateFormated = startDate ? format(new Date(startDate), DATE_FORMAT) : null
-  const endDateFormated = endDate ? format(new Date(endDate), DATE_FORMAT) : 'now'
+  const startDateFormated = startDate
+    ? format(new Date(startDate), DATE_FORMAT, { locale: localeMap[locale] })
+    : null
+  const endDateFormated = endDate
+    ? format(new Date(endDate), DATE_FORMAT, { locale: localeMap[locale] })
+    : t('now')
 
   return (
     <div className={clsx(styles.timeLineEntry, { [styles.animate]: isIntersecting })} ref={ref}>
