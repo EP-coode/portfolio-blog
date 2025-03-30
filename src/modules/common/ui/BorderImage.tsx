a35f7b167ab0911514531719101bc1160794508b
Ernest Przybył <ernestprzybyl@gmail.com>
2025-03-30 18:58:08 +0200

    small style fixed, get rid of crappy next image from blog

--- src/modules/common/ui/BorderImage.tsx
+++ src/modules/common/ui/BorderImage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { Media } from '@/payload-types'
-import Image from 'next/image'
+// import Image from 'next/image'
 
 import styles from './BorderImage.module.css'
 
@@ -13,15 +13,7 @@ const BorderImage = ({ media, caption }: { media: Media; caption?: string }) => 
     <figure className={styles.figure}>
       <div className={styles.mediaBlockWrapper} style={{ width: '100%', aspectRatio: aspectRatio }}>
         {media?.thumbnailURL}
-        <Image
-          src={imgSrc}
-          alt={media?.alt || ''}
-          width={0}
-          height={0}
-          style={{ width: '100%', height: '100%' }}
-          className={styles.mediaBlock}
-          quality={100}
-        ></Image>
+        <img src={imgSrc} alt={media?.alt || ''} className={styles.mediaBlock}></img>
       </div>
       {caption && <div className={styles.caption}>{caption}</div>}
     </figure>
