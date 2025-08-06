2c51b788fffcd4957615cbd119d6d026c81440c0
Ernest Przybył <ernestprzybyl@gmail.com>
2025-08-06 21:11:44 +0200

    set cache header for fonts

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,17 @@ const withNextIntl = createNextIntlPlugin()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: '/fonts/:fontPath*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable', // Cache for 1 year
+        },
+      ],
+    },
+  ],
 }
 
 export default withNextIntl(withPayload(nextConfig))
